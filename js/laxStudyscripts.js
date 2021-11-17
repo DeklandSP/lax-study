5fca4a42fc8536caef985b9709b228eca92011e2
Lewis Traynor <94173673+LewisTraynor@users.noreply.github.com>
2021-11-17 17:42:58 +0000

    Add files via upload

--- js/laxStudyscripts.js
+++ js/laxStudyscripts.js
@@ -7,12 +7,13 @@ function hideButton() {
     } else {
         buttonHidden.style.display = "block";
     }
-    var timeLeft = 59;
+    var timeLeft = 9;
     var menu1 = document.getElementById("continue")
     var downloadTimer = setInterval(function () {
         if (timeLeft == 0) {
             if (menu1.style.display === "none") {
                 menu1.style.display = "block";
+                boxLoop.style.display = "none";
             }
             else {
                 menu1.style.display = "none";
@@ -25,19 +26,50 @@ function hideButton() {
 }
 
 
-
-
 function continueMeditation() {
     var menu1 = document.getElementById("continue")
+    var boxLoop = document.getElementById("box1")
     if (menu1.style.display === "none") {
         menu1.style.display = "block";
+    } else if(menu1.style.display === "block"){
+        menu1.style.display = "none";
+        boxLoop.style.display = "block";
+    }else{
+        menu1.style.display = "none";
+    }
+    var timeLeft = 9;
+    var menu1 = document.getElementById("continue")
+    var downloadTimer = setInterval(function () {
+        if (timeLeft == 0) {
+            if (menu1.style.display === "none") {
+                menu1.style.display = "block";
+                boxLoop.style.display = "none";
+            }
+            else {
+                menu1.style.display = "none";
+            }
+        } else {
+        }
+        timeLeft -= 1;
+    }, 1000);
+
+}
+
+function stopMeditation() {
+    var buttonHidden = document.getElementById("meditationButton")
+    var menu1 = document.getElementById("continue")
+    if (buttonHidden.style.display === "block") {
+        buttonHidden.style.display = "none";
+        boxLoop.style.display = "block";
     } else {
+        buttonHidden.style.display = "block";
+    }
+    if (menu1.style.display === "none") {
+        menu1.style.display = "block";
+    } else if(menu1.style.display === "block"){
+        menu1.style.display = "none";
+        boxLoop.style.display = "block";
+    }else{
         menu1.style.display = "none";
     }
 }
-
-
-
-//function stopMeditation() {
-
-//}
